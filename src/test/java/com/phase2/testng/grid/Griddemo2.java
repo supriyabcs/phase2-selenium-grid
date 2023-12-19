c5f23d698549c8ebb2b440f7ba189e71a51bcd9b
supriyabcs <supriyachandru2000@gmail.com>
2023-12-19 16:54:30 +0530

    selenium grid

---
+++ src/test/java/com/phase2/testng/grid/Griddemo2.java
@@ -0,0 +1,23 @@
+package com.phase2.testng.grid;
+import java.net.MalformedURLException;
+
+import org.testng.annotations.Test;
+
+public class Griddemo2 extends HubandNodeDemo {
+	
+	
+	@Test(priority='1')
+	public void chromeTest1() throws MalformedURLException
+	{
+		hubNodeChromeDemo();
+		driver.get("https://www.selenium.dev/downloads/");
+	}
+	
+	@Test(priority='2')
+	public void firefoxTest1() throws MalformedURLException
+	{
+		hubNodeFirefoxDemo();
+		driver.get("https://www.selenium.dev/downloads/");
+	}
+
+}
---
+++ src/test/java/com/phase2/testng/grid/StandAloneChrome.java
@@ -0,0 +1,46 @@
+package com.phase2.testng.grid;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class StandAloneChrome {
+	
+	
+WebDriver driver;
+	
+	@BeforeClass
+	public void gridstandAlone_Chrome() throws MalformedURLException
+	{
+	// Write the deisred configuration where the script has to run
+		
+	   ChromeOptions cap = new ChromeOptions();
+		
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+		
+	}
+	
+	@Test
+	public void gridstandalone_Test()
+	{
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		String expectedTitle="Your Store";
+		String ActualTitle = driver.getTitle();
+		System.out.println(ActualTitle);
+		Assert.assertEquals(ActualTitle, expectedTitle);
+	}
+	
+   @AfterClass
+   public void closesession()
+   {
+	   driver.quit();
+   }
+
+}
